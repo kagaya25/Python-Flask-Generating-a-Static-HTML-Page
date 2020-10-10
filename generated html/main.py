9e51b842b6c0af43878a0b2f568da38fb6815b86
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-10-11 01:04:07 +0900

    update the
    update

--- generated html/main.py
+++ generated html/main.py
@@ -1,11 +1,25 @@
+from flask import Flask
+
 from flask import render_template
-import flask
 
-app = flask.Flask('my app')
 
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+   return render_template('blog/blog.html')
+   
 if __name__ == "__main__":
     with app.app_context():
         rendered = render_template('blog.html', \
             title = "My Generated Page", \
             people = [{"name": "kagaya"}, {"name": "john"}])
-        print(rendered)
+        lines = []
+
+        with open('templates/blog/blog.html', 'w') as f:
+        	f.write(rendered)
+    app.run(debug = True,port=4000)
+	
+		
+
+
+		
